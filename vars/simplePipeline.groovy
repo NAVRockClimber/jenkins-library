43e5dbc19c391063355c3467860d40a54ec92af3
Your Name <you@example.com>
2023-04-10 16:27:51 +0200

    test

---
+++ vars/simplePipeline.groovy
@@ -0,0 +1,14 @@
+def call() {
+    
+    pipeline {
+        agent {kubernetes}
+        stages {
+            stage("hello") {
+                steps {
+                    sh "echo hello world"
+                }
+            }
+
+        }
+    }
+}
